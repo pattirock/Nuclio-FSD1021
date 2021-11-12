cd7932177f191c0f32ce718b038b5b0d1a4e1ae8
pattirock <pmateosromero@gmail.com>
2021-11-12 13:57:04 +0100

    Open the modal with the info

--- src/pages/list/card/card.js
+++ src/pages/list/card/card.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './card.module.css';
 
-const Card = ({ item }) => (
-  <div className={styles.container} style={{ backgroundImage: `url(${item.thumbnailUrl})` }}>
+const Card = ({ item, onClick }) => (
+  <div className={styles.container} style={{ backgroundImage: `url(${item.thumbnailUrl})` }} onClick={onClick}>
     <div className={styles.info}>
       <p><b>{item.title}</b></p>
     </div>
--- src/pages/list/list.js
+++ src/pages/list/list.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Card from './card/card';
 import data from '../../assets/response.json';
 import styles from './list.module.css';
+import Modal from '../../components/modal/modal';
 
 const List = ({ searchValue }) => {
+  const [selectedItem, setSelectedItem] = useState();
   const [filteredData, setFilteredData] = useState(data);
   useEffect(() => {
     if (searchValue !== '') {
@@ -16,7 +18,14 @@ const List = ({ searchValue }) => {
 
   return (
     <div className={styles.container}>
-      {filteredData.map(item => <Card item={item} />)}
+      {filteredData.map(item => <Card item={item} onClick={() => {
+        setSelectedItem(item);
+      }} />)}
+      {selectedItem && <Modal onClose={() => setSelectedItem(null)}>
+        <p><b>Title:</b> {selectedItem.title}</p>
+        <p><b>Url:</b> {selectedItem.url}</p>
+        <img src={selectedItem.thumbnailUrl} width="400" />
+      </Modal>}
     </div>
   );
 };
