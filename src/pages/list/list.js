40f3fa54cbed2d6c004e2d5add0cc97ffd98b8e2
pattirock <pmateosromero@gmail.com>
2021-11-12 14:06:33 +0100

    Api request

--- src/pages/list/list.js
+++ src/pages/list/list.js
@@ -6,7 +6,7 @@ import Modal from '../../components/modal/modal';
 
 const List = ({ searchValue }) => {
   const [selectedItem, setSelectedItem] = useState();
-  const [filteredData, setFilteredData] = useState(data);
+  const [filteredData, setFilteredData] = useState([]);
   useEffect(() => {
     if (searchValue !== '') {
       setFilteredData(data.filter(({ title }) => {
@@ -16,12 +16,18 @@ const List = ({ searchValue }) => {
     }
   }, [searchValue, setFilteredData]);
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=100')
+      .then(response => response.json())
+      .then(json => setFilteredData(json))
+  }, []);
+
   return (
     <div className={styles.container}>
       {filteredData.map(item => <Card item={item} onClick={() => {
         setSelectedItem(item);
       }} />)}
-      {selectedItem && <Modal onClose={() => setSelectedItem(null)}>
+      {!!selectedItem && <Modal onClose={() => setSelectedItem(null)}>
         <p><b>Title:</b> {selectedItem.title}</p>
         <p><b>Url:</b> {selectedItem.url}</p>
         <img src={selectedItem.thumbnailUrl} width="400" />
